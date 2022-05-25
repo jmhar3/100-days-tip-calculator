2f01b0bd7387f137e77b47ffba413c33929d9501
jmhar <jmhar@protonmail.com>
2022-05-25 22:17:17 +1000

    set total background + icons

--- tip-calculator/src/components/BillTotal.js
+++ tip-calculator/src/components/BillTotal.js
@@ -1,8 +1,13 @@
+import iconDollar from "../images/icon-dollar.svg";
+
 export const BillTotal = () => {
   return (
     <div>
       <p>Bill</p>
-      <input type="decimal" />
+      <div className="input-container">
+        <img src={iconDollar} />
+        <input type="decimal" placeholder="0" />
+      </div>
     </div>
   );
 };
--- tip-calculator/src/components/PeopleTotal.js
+++ tip-calculator/src/components/PeopleTotal.js
@@ -1,8 +1,13 @@
+import iconPerson from "../images/icon-person.svg";
+
 export const PeopleTotal = () => {
- return (
-   <div>
-     <p>Number of people</p>
-     <input type="number" />
-   </div>
- );
+  return (
+    <div>
+      <p>Number of people</p>
+      <div className="input-container">
+        <img src={iconPerson} />
+        <input type="number" placeholder="0" />
+      </div>
+    </div>
+  );
 };
