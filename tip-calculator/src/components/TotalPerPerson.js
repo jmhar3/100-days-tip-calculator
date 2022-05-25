3170c4b5f9ca56cf91ee94a40e6083a3f54f3fde
jmhar <jmhar@protonmail.com>
2022-05-25 21:46:34 +1000

    Style Per Person components + remove all padding, margin:

--- tip-calculator/src/components/TotalPerPerson.js
+++ tip-calculator/src/components/TotalPerPerson.js
@@ -1,11 +1,11 @@
 export const TotalPerPerson = () => {
  return (
-   <div>
+   <div className="per-person">
      <div>
        <h4>Total</h4>
        <p>/ person</p>
      </div>
-     <h1>$.0.00</h1>
+     <h1>$0.00</h1>
    </div>
  );
 };
